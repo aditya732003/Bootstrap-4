8bf30ac1a8b8cf681c7b5e428ec3a07c4051a244
aditya732003 <adityasharma@webiwork.com>
2021-10-26 19:24:44 +0530

    Front

--- Front/js/custom.js
+++ Front/js/custom.js
@@ -120,4 +120,11 @@ $('#nav-icon').click(function(){
     $(this).toggleClass('open');
   });
 
+$('.dropdown').on('show.bs.dropdown', function(e){
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+});
+
+$('.dropdown').on('hide.bs.dropdown', function(e){
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp(300);
+});
 });
