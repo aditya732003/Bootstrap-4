7acdb060a88a91a37d856c52232126028397f1fb
aditya732003 <adityasharma@webiwork.com>
2021-12-18 14:55:23 +0530

    Shopamp

--- Shopamp/js/custom.js
+++ Shopamp/js/custom.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 	$(".navbar-toggler").click(function () {
     $(".sidebar").addClass("open");
+    $(".body").addClass("open");
 });
 	$(".close-sidebar").click(function () {
     $(".sidebar").removeClass("open");
+    $(".body").removeClass("open");
 });
 });
